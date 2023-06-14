9ea0d2446b910edc474bd3c222307de7c6010fa7
Pingin Ngepot <24736574+bowors@users.noreply.github.com>
2023-06-15 04:46:39 +0700

    Hajar bleh

--- server.sh
+++ server.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install libevhtp-dev libevent-dev libjansson-dev
-git clone https://github.com/bowors/GlitchGalore.git
-cd /content/GlitchGalore
-gcc -o server server.c -levhtp -levent -ljansson
-chmod +x server
-
 # Check if the keys directory is present
 if [[ ! -d /content/drive/MyDrive/keys ]]; then
     echo "Error: keys directory not found"
     exit 1
+fi
+
+# Check if Flask is already installed and install it if not
+if ! python3 -c "import flask" &> /dev/null; then
+    pip install flask
 fi
 
 # Create .ssh directory and copy keys into it
@@ -20,14 +19,15 @@ cp /content/drive/MyDrive/keys/* ~/.ssh
 if [[ -e mypipe ]]; then
     rm mypipe
 fi
-
 mkfifo mypipe
+
 
 # Define process 1: Run the Flask web server
 process1() {
-    ./server
+    python3 -c app.py 
 }
 
+# Define process 2: Keep track of subprocesses and download file when notified by process 1
 # Define process 2: Keep track of subprocesses and download file when notified by process 1
 process2() {
     pids=()
@@ -39,23 +39,13 @@ process2() {
             done
             pids=()
         fi
-        case $line in
-            "request received from /hook/1")
-                wget -qO- https://example.com >&2 || \
-                echo "Error: wget command failed" &
-                pids+=($!)
-                ;;
-            "request received from /hook/2")
-                wget -qO- https://google.com >&2 || \
-                echo "Error: wget command failed" &
-                pids+=($!)
-                ;;
-            "request received from /hook/3")
-                wget -qO- https://bing.com >&2 || \
-                echo "Error: wget command failed" &
-                pids+=($!)
-                ;;
-        esac
+        hook_id=$(echo $line | grep -oP '(?<=/hook/)[0-9]+')
+        if [[ -n $hook_id ]]; then
+            url=${urls[$((hook_id-1))]}
+            wget -qO- $url >&2 || \
+            echo "Error: wget command failed" &
+            pids+=($!)
+        fi
     done
 }
 
