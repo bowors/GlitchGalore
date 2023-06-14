2313dd6686892b7b7cdfb9b70bb9e51ee2328697
Pingin Ngepot <24736574+bowors@users.noreply.github.com>
2023-06-15 03:58:30 +0700

    Update server.c

--- server.c
+++ server.c
@@ -1,52 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
-#define RESPONSE_200 "HTTP/1.1 200 OK\r\n\r\n"
-#define RESPONSE_404 "HTTP/1.1 404 Not Found\r\n\r\n"
+#define DEFAULT_RESPONSE "Hello world!"
 
-void handle_get_request(const char* filename, int client_socket) {
-    FILE* file = fopen(filename, "r");
-    if (file != NULL) {
-        char response[BUFFER_SIZE];
-        snprintf(response, BUFFER_SIZE, "%s", RESPONSE_200);
-        send(client_socket, response, strlen(response), 0);
-        
-        char buffer[BUFFER_SIZE];
-        size_t bytesRead;
-        while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
-            send(client_socket, buffer, bytesRead, 0);
+void serve_get_request(const char* file_path, int client_socket) {
+    FILE* file;
+    char response[BUFFER_SIZE];
+
+    if (file_path == NULL) {
+        file = fopen("index.html", "r");
+        if (file == NULL) {
+            write(client_socket, DEFAULT_RESPONSE, strlen(DEFAULT_RESPONSE));
+            return;
         }
-        
-        fclose(file);
     } else {
-        char response[BUFFER_SIZE];
-        snprintf(response, BUFFER_SIZE, "%sFile Not Found", RESPONSE_404);
-        send(client_socket, response, strlen(response), 0);
+        file = fopen(file_path, "r");
+        if (file == NULL) {
+            write(client_socket, DEFAULT_RESPONSE, strlen(DEFAULT_RESPONSE));
+            return;
+        }
     }
+
+    sprintf(response, "HTTP/1.1 200 OK\r\n\r\n");
+    write(client_socket, response, strlen(response));
+
+    while (fgets(response, BUFFER_SIZE, file) != NULL) {
+        write(client_socket, response, strlen(response));
+    }
+
+    fclose(file);
 }
 
-void handle_post_request(const char* id, int client_socket) {
-    printf("Received POST request with ID: %s\n", id);
-    char response[] = RESPONSE_200;
-    send(client_socket, response, strlen(response), 0);
+void serve_post_request(const char* id, int client_socket) {
+    char response[BUFFER_SIZE];
+    sprintf(response, "HTTP/1.1 200 OK\r\n\r\n");
+    write(client_socket, response, strlen(response));
+
+    printf("Received POST request with id: %s\n", id);
 }
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        printf("Usage: %s <html_file>\n", argv[0]);
+        printf("Please provide an HTML file as an argument.\n");
         return 1;
     }
-    
-    const char* filename = argv[1];
-    
-    // TODO: Implement the HTTP server logic here, handling GET and POST requests
-    
-    // Example code to forward the HTTP server using SSH
-    char command[BUFFER_SIZE];
-    snprintf(command, BUFFER_SIZE, "ssh -T -o StrictHostKeyChecking=no -R pingin-ngepot:80:localhost:8080 serveo.net");
-    system(command);
-    
+
+    // Extract the file name from the argument
+    const char* file_name = argv[1];
+
+    // Set up a simple HTTP server
+    int server_socket, client_socket;
+    char request[BUFFER_SIZE];
+    char method[BUFFER_SIZE];
+    char url[BUFFER_SIZE];
+    char protocol[BUFFER_SIZE];
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
+    struct sockaddr_in server_address, client_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(8080);
+
+    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        perror("Socket binding failed");
+        return 1;
+    }
+
+    if (listen(server_socket, 5) == -1) {
+        perror("Socket listening failed");
+        return 1;
+    }
+
+    printf("Server listening on port 8080...\n");
+
+    while (1) {
+        socklen_t client_address_length = sizeof(client_address);
+        client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_length);
+        if (client_socket == -1) {
+            perror("Failed to accept client connection");
+            return 1;
+        }
+
+        read(client_socket, request, BUFFER_SIZE);
+
+        sscanf(request, "%s %s %s", method, url, protocol);
+
+        if (strcmp(method, "GET") == 0 && strcmp(url, "/") == 0) {
+            serve_get_request(file_name, client_socket);
+        } else if (strcmp(method, "POST") == 0) {
+            // Extract the id from the URL
+            char* id_start = strstr(url, "/hook/");
+            if (id_start != NULL) {
+                char id[BUFFER_SIZE];
+                sscanf(id_start, "/hook/%s", id);
+                serve_post_request(id, client_socket);
+            } else {
+                char response[BUFFER_SIZE];
+                sprintf(response, "HTTP/1.1 404 Not Found\r\n\r\n");
+                write(client_socket, response, strlen(response));
+            }
+        } else {
+            char response[BUFFER_SIZE];
+            sprintf(response, "HTTP/1.1 404 Not Found\r\n\r\n");
+            write(client_socket, response, strlen(response));
+        }
+
+        close(client_socket);
+    }
+
+    close(server_socket);
+
     return 0;
 }
