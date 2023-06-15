91674a7ceb6aea0630257506be18afdd0fe9b5ea
Pingin Ngepot <24736574+bowors@users.noreply.github.com>
2023-06-15 15:33:38 +0700

    Auto-committing 'app.py'

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
-# from flask import Flask, request
-# import json
+from flask import Flask, request
+import json
 
-# with open('index.html', 'r') as f:
-#     html_content = f.read()
+with open('index.html', 'r') as f:
+    html_content = f.read()
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/', methods=['GET'])
-# def home():
-#      return html_content
+@app.route('/', methods=['GET'])
+def home():
+     return html_content
 
-# @app.route('/hook/<int:hook_id>', methods=['POST'])
-# def index(hook_id):
-#     # Notify process 2 by writing to the named pipe
-#     with open('mypipe', 'w') as f:
-#         f.write(f"request received from /hook/{hook_id}\n")
-#     # Format JSON output nicely and print it
-#     try:
-#         data = json.loads(request.data)
-#         print(json.dumps(data, indent=4))
-#     except:
-#         print(request.data)
+@app.route('/hook/<int:hook_id>', methods=['POST'])
+def index(hook_id):
+    # Notify process 2 by writing to the named pipe
+    with open('mypipe', 'w') as f:
+        f.write(f"request received from /hook/{hook_id}\n")
+    # Format JSON output nicely and print it
+    try:
+        data = json.loads(request.data)
+        print(json.dumps(data, indent=4))
+    except:
+        print(request.data)
     
-#     return 'Hello world'
+    return 'Hello world'
 
-# if __name__ == '__main__':
-#     app.run(port=5150)
-
-print "asu xxx"
+if __name__ == '__main__':
+    app.run(port=5150)
